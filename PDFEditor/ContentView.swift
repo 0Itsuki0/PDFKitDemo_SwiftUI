f0b673fcef9ccaf3a29a6575be340df9582eb69d
itsuki <itsuki.enjyoy@gmail.com>
2024-12-28 09:07:57 +0900

    Initial Commit

---
+++ PDFEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDFEditor
+//
+//  Created by Itsuki on 2024/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDFEditor/PDFEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFEditorApp.swift
+//  PDFEditor
+//
+//  Created by Itsuki on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct PDFEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
